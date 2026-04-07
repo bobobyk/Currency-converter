4872ddbe4f87fc3a69b2328c08d79062d32df6f6
frexk <149825556+Akumaqqe12@users.noreply.github.com>
2026-04-07 09:38:16 +0300

    Update MainWindow.xaml.cs

--- Converter_value/MainWindow.xaml.cs
+++ Converter_value/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Converter_value
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
     }
 }
